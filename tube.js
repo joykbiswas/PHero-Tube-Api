8c613fe4eaa502600d5c09a108745b0cb71de955
joykbiswas <joykumarbiswas100@gmail.com>
2023-09-01 22:48:24 +0600

    compite resposive

--- tube.js
+++ tube.js
@@ -29,7 +29,7 @@ const handleButton = async()=>{
     const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await res.json()
     const AllButton = data.data
-    // console.log(AllButton);
+    console.log(AllButton);
 
 
     const buttonContainer= document.getElementById('button-container')
@@ -49,31 +49,44 @@ const handleLoadData = async (id) =>{
      
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML= ''
+   if(cardData.length >0){
     cardData.forEach((data)=>{
-        console.log(data);
-    const div = document.createElement('div')
-    div.innerHTML=`
-    <div class="card  bg-base-100 shadow-xl">
-    <figure><img class="h-48" src="${data.thumbnail}" alt="img" /></figure>
-    <div class="card-body flex flex-row">
-      <div class="avatar ">
-         <div class="w-14 h-14 rounded-full">
-          <img class="" src="${data.authors[0].profile_picture}">
-         </div>
-      </div>
-      <div>
-      <h2 class="card-title"> ${data.authors[0].profile_name}   <span><img src="./logo/fi_10629607.svg" ${data.authors[0].verified ? "" : "hidden"}></span>
-      </h2>
-      <p>${data.others.views} views</p>
-        </div>
-    
-      </div>
-      
+      console.log(data);
+  const div = document.createElement('div')
+  div.innerHTML=`
+  <div class="card  bg-base-100 shadow-xl">
+  <figure><img class="h-48" src="${data.thumbnail}" alt="img" /></figure>
+  <div class="card-body flex flex-row">
+    <div class="avatar ">
+       <div class="w-14 h-14 rounded-full">
+        <img class="" src="${data.authors[0].profile_picture}">
+       </div>
     </div>
+    <div>
+    <h2 class="card-title"> ${data.authors[0].profile_name}   <span><img src="./logo/fi_10629607.svg" ${data.authors[0].verified ? "" : "hidden"}></span>
+    </h2>
+    <p>${data.others.views} views</p>
+      </div>
+  
+    </div>
+    
   </div>
-    `;
-    cardContainer.appendChild(div)
-    })
+</div>
+  `;
+  cardContainer.appendChild(div)
+  })
+
+   }else{
+
+    const cardContainer = document.getElementById('card-container')
+
+    cardContainer.innerHTML=`
+       <div class=" w-full m-auto  ">
+       <img class="w-40 m-auto" src="./logo/Icon.png" alt="img" />
+       <h3 class="text-xl text-center">Oops!! Sorry, There is <br> no content here</h3>
+       </div>
+      `
+   }
 
 
 }
